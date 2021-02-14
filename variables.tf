d87b45a146f7debe49696762ea2cc562e5f892b7
Derek Morgan <derekm1215@gmail.com>
2021-02-14 01:44:39 +0000

    first commit

---
+++ variables.tf
@@ -0,0 +1,67 @@
+locals {
+  security_groups = {
+    public = {
+      name        = "public_sg"
+      description = "public access"
+      ingress = {
+        open = {
+          from        = 0
+          to          = 0
+          protocol    = -1
+          cidr_blocks = [var.access_ip]
+        }
+        tg = {
+          from        = 8000
+          to          = 8000
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+        http = {
+          from        = 80
+          to          = 80
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      }
+    }
+    rds = {
+      name        = "rds_sg"
+      description = "rds access"
+      ingress = {
+        mysql = {
+          from        = 3306
+          to          = 3306
+          protocol    = "tcp"
+          cidr_blocks = [local.vpc_cidr]
+        }
+      }
+    }
+  }
+}
+
+variable "vpc_cidr" {
+  default = "10.123.0.0/16"
+}
+variable "public_cidrs" {
+  default = ["10.123.1.0/24", "10.123.3.0/24"]
+}
+variable "private_cidrs" {
+  default = ["10.123.2.0/24", "10.123.4.0/24", "10.123.6.0/24"]
+}
+variable "public_sn_count" {
+  default = 2
+}
+variable "private_sn_count" {
+  default = 3
+}
+variable "max_subnets" {
+  default = 5
+}
+variable "access_ip" {
+  default = ["0.0.0.0/0"]
+}
+variable "security_groups" {}
+
+variable "db_subnet_group" {
+  default = true
+}
